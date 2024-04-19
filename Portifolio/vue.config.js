5ccd5bbe42052e1b91360fbba8945a45e50961a5
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2024-04-18 23:49:19 -0300

    att

--- Portifolio/vue.config.js
+++ Portifolio/vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? 'Projetos-publicos/Portifolio/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/Projetos-publicos/Portifolio/' : '/'
 };
