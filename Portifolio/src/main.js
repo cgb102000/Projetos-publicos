4300491757bd888b69d3fe087fcdf6dc1e43822e
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2024-04-18 23:49:19 -0300

    att

--- Portifolio/src/main.js
+++ Portifolio/src/main.js
@@ -1,17 +1,9 @@
-import './assets/main.css'
+import '/Projetos-publicos/Portifolio/assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
 
-import App from './App.vue'
-
-
-
-
-const app = createApp(App)
-
-app.use(createPinia())
-
-
-app.mount('#app')
-
+const app = createApp(App);
+app.use(createPinia());
+app.mount('#app');
