1afcd1bd0572d98cdc1f83a4bf4b394443273cf6
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2024-10-24 21:49:42 -0300

    ...

--- Portifolio/main.js
+++ Portifolio/main.js
@@ -1,7 +1,7 @@
-import './assets/main.css';
+import './src/assets/main.css'; // Caminho correto para o CSS
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import App from './App.vue';
+import App from './src/App.vue'; // Caminho correto para o App.vue
 
 const app = createApp(App);
 app.use(createPinia());
